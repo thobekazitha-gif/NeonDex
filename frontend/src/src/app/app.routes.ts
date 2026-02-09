d921c8dbdc79adab66fbf8cb8b4b1a0b38f4a1b8
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-09 13:09:50 +0200

    Implementing files

---
+++ frontend/src/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+
+export const routes: Routes = [
+  { path: '', component: PokemonListComponent },           // ← this line must be present
+  { path: 'pokemon/:name', component: PokemonDetailComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
