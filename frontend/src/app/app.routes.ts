4474862380390df1955c7606245f45e668cdddc5
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-09 11:26:15 +0200

    Updating app.css file format

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
-import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
-
-export const routes: Routes = [
-  { path: '', component: PokemonListComponent },
-  { path: 'pokemon/:name', component: PokemonDetailComponent },
-  { path: '**', redirectTo: '' }
-];
