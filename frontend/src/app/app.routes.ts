a984acf0ad8a97014e7548898aae8eb9bfbd339f
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-09 13:15:33 +0200

    Updating files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+
+export const routes: Routes = [
+  { path: '', component: PokemonListComponent },           // ← this line must be present
+  { path: 'pokemon/:name', component: PokemonDetailComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
